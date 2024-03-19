b1490ec6d86e8cf5cb9a1806b3283d73ad1d9704
Aishwarya Iyer <90432405+ryaready@users.noreply.github.com>
2024-03-19 14:32:48 +0800

    chanhge

--- MySplashScreen/app/src/main/java/com/example/mysplashscreen/SplashActivity.java
+++ MySplashScreen/app/src/main/java/com/example/mysplashscreen/SplashActivity.java
@@ -16,12 +16,21 @@ public class SplashActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.animatedImageView);
 
         // Set animation to ImageView
-        imageView.setBackgroundResource(R.drawable.animation_splash);
+        imageView.setBackgroundResource(R.drawable.animation_splash); // Use the scaled animation drawable
 
         // Get AnimationDrawable object
         AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getBackground();
+
+        // Calculate scaling factor
+        float scaleFactor = 5f; // Scale factor of 5
+
+        // Set the duration of each frame according to the scaling factor
+        for (int i = 0; i < animationDrawable.getNumberOfFrames(); i++) {
+            animationDrawable.getFrame(i).setLevel((int) (animationDrawable.getFrame(i).getLevel() * scaleFactor));
+        }
 
         // Start the animation
         animationDrawable.start();
     }
 }
+
