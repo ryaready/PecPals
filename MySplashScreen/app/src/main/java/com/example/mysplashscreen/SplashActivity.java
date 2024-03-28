28defdf55bf2bd58c9a6a1f086439002a32ce0c1
Aishwarya Iyer <90432405+ryaready@users.noreply.github.com>
2024-03-28 19:28:09 +0800

    yay login first

--- MySplashScreen/app/src/main/java/com/example/mysplashscreen/SplashActivity.java
+++ MySplashScreen/app/src/main/java/com/example/mysplashscreen/SplashActivity.java
@@ -42,7 +42,7 @@ public class SplashActivity extends AppCompatActivity {
         // Using a Handler to delay the transition
         new Handler().postDelayed(() -> {
             // Start BottomNavActivity after SPLASH_TIME
-            Intent intent = new Intent(SplashActivity.this, SignupActivity.class);
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
             startActivity(intent);
             finish(); // Finish SplashActivity
         }, SPLASH_TIME);
