cf8de4c63cf6b18956664f1bf8cb3733a97898ff
Anika Handigol <anikaajayhandigol@gmail.com>
2024-03-28 17:25:36 +0800

    Taskactivity
    added 3 java files: TaskActivity.java, VeiwPagerItem.java, VPAdapter.java, edited string and colour xml files, added drawables a,b,c,d,e.

---
+++ Shop/app/src/main/java/com/example/store1/MyAdapter.java
@@ -0,0 +1,66 @@
+package com.example.store1;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<CoinItems> coinItemsArrayList;
+
+    public MyAdapter(Context context, ArrayList<CoinItems> coinItemsArrayList) {
+        this.context = context;
+        this.coinItemsArrayList = coinItemsArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        CoinItems coinitems = coinItemsArrayList.get(position);
+        holder.description.setText(coinitems.description);
+        holder.price.setText(coinitems.price);
+        holder.image.setImageResource(coinitems.image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return coinItemsArrayList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView description;
+        TextView price;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.imageView);
+            description = itemView.findViewById(R.id.description);
+            price = itemView.findViewById(R.id.price);
+            itemView.findViewById(R.id.buybutton).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("button", "onClick user wants to buy" + description );
+
+                }
+            });
+        }
+    }
+}
