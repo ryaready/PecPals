29829a3107e7dfb7744d8b97f4ef44b6a1c0949b
Orangelephant1212 <161124060+Orangelephant1212@users.noreply.github.com>
2024-03-28 17:27:20 +0800

    Shop java, xml, png files

---
+++ RealItems.java
@@ -0,0 +1,15 @@
+package com.example.store1;
+
+public class RealItems {
+
+    String reward;
+    String rewarddescription;
+
+    int rewardimage;
+
+    public RealItems(String reward, String rewarddescription, int rewardimage) {
+        this.reward = reward;
+        this.rewarddescription = rewarddescription;
+        this.rewardimage = rewardimage;
+    }
+}
