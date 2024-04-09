c9057bea60fb9744cde6a5ec46e53eaa89d1b88d
Aishwarya Iyer <90432405+ryaready@users.noreply.github.com>
2024-04-09 10:36:42 +0800

    observe

--- MySplashScreen/app/src/main/java/com/example/mysplashscreen/ProfileFragment.java
+++ MySplashScreen/app/src/main/java/com/example/mysplashscreen/ProfileFragment.java
@@ -1,5 +1,4 @@
 package com.example.mysplashscreen;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,14 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.mysplashscreen.databinding.FragmentProfileBinding;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements UserObserver {
 
     private FragmentProfileBinding binding;
     private SharedPreferences sharedPreferences;
+
+    User user = User.getInstance();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -23,26 +27,24 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        TextView usernameTextView = view.findViewById(R.id.textView);
+        return binding.getRoot();
+    }
 
-        User user = User.getInstance();
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        updateUI();
 
-        String username = user.getInstance().getEmail();
-
+        String username = user.getEmail();
+        TextView usernameTextView = binding.textView;
         usernameTextView.setText("@" + username);
-
-        TextView coinTextView = view.findViewById(R.id.textView2);
-
-        String coins = String.valueOf(user.getInstance().getCoins());
-
-        coinTextView.setText("coins: "+ coins);
-
 
 
         // Set click listener for the logout button
         binding.logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 // Mark user as logged out
 
                 // Redirect to Login Activity
@@ -53,8 +55,27 @@ public class ProfileFragment extends Fragment {
                 requireActivity().finishAffinity();
             }
         });
-
-        return view;
     }
 
+    // Update UI with current user data
+    private void updateUI() {
+
+        String coins = String.valueOf(user.getCoins());
+        TextView coinTextView = binding.textView2;
+        coinTextView.setText("coins: " + coins);
+    }
+
+    // Implement observer methods to handle updates
+    @Override
+    public void onUserUpdated(User user) {
+        updateUI();
+    }
+
+    // Don't forget to unregister the observer when the fragment is destroyed
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Unregister this fragment as an observer of the User class
+        User.getInstance().removeObserver(this);
+    }
 }
--- MySplashScreen/app/src/main/java/com/example/mysplashscreen/UserObserver.java
+++ MySplashScreen/app/src/main/java/com/example/mysplashscreen/UserObserver.java
@@ -2,7 +2,5 @@ package com.example.mysplashscreen;
 
 
 public interface UserObserver {
-    void onUserXpUpdated(User user);
-    void onUserCoinsUpdated(User user);
-    void onUserLoginStreakUpdated(User user);
+    void onUserUpdated(User user);
 }
